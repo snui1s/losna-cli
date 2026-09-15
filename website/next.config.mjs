9ea20a7691bbb71402363b8d766c291820ca07ce
snui1s <snailsqz@gmail.com>
2026-09-15 11:17:04 +0700

    chore: build website static export into docs folder

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: '../docs',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || (isProd ? '/losna-cli' : ''),
 };
 
 export default nextConfig;
